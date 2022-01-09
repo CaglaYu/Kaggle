883876bfb86b8c99355d134e4f22cf3e81b2007d
CaglaYu <caglasi@hotmail.com>
2022-01-09 16:42:31 +0100

    4th

---
+++ history.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Jan  8 20:02:53 2022)---
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+debugfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+
+## ---(Sat Jan  8 21:09:33 2022)---
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+!git add "temp.py"
+
+## ---(Sat Jan  8 21:25:05 2022)---
+!git add "temp.py"
+!git init
+!git add "temp.py"
+!git fetch
+!git add "temp.py"
+echo "# Kaggle" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/CaglaYu/Kaggle.git
+git push -u origin main
+echo "# Kaggle" >> README.md
+git init
+!git init
+!git add readme.md
+!git commit -m "first commit"
+!git config --global user.name "CaglaYu"
+!git commit -m "first commit"
+!git config --global user.email "caglasi@hotmail.com"
+!git commit -m "first commit"
+!git add "temp.py"
+!git add temp.py
+dir
+cd users
+cd .spyder-py3
+!git init
+!git add "temp.py"
+!git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/CaglaYu/Kaggle.git
+git push -u origin main
+git branch -M main
+!git branch -M main
+!git remote add origin https://github.com/CaglaYu/Kaggle.git
+!git push -u origin main
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+
+## ---(Sun Jan  9 13:39:22 2022)---
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+conda create -n spyder-env -y
+conda activate spyder-env
+conda install spyder-kernels scikit-learn -y
+conda create -n spyder-env -y
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+
+## ---(Sun Jan  9 13:48:12 2022)---
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+
+## ---(Sun Jan  9 15:38:02 2022)---
+runfile('C:/Users/cagla/.spyder-py3/temp.py', wdir='C:/Users/cagla/.spyder-py3')
+!git commit -m "third commit"
+!git add temp.py
+!git commit -m "third commit"
+!git push -u origin main
