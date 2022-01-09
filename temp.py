9b76a75f10f2b99f44dccbab1ca1722643b07442
CaglaYu <caglasi@hotmail.com>
2022-01-09 16:42:03 +0100

    tt

--- temp.py
+++ temp.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
 
 iowa_file_path = 'train.csv'
 
@@ -21,3 +23,15 @@ my_model.fit(X, y)
 print(X.head())
 my_predictions = my_model.predict(X)
 print(mean_absolute_error(y, my_predictions))
+
+
+
+#redfine model with evaluation using splitting:
+#Until now, we have evaluated the model with the data which we used to form the model in the first placr
+#Now we split the data and use one part to form a model and the other part to evaluate the model:
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+my_model = DecisionTreeRegressor()
+my_model.fit(train_X, train_y)
+my_predictions = my_model.predict(val_X)
+print(mean_absolute_error(val_y, my_predictions))
+
