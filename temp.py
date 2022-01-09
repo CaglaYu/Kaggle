ab5f0a042a557f7143df0a668f55be0840616fe8
CaglaYu <caglasi@hotmail.com>
2022-01-09 13:52:53 +0100

    second

--- temp.py
+++ temp.py
@@ -1,7 +1,21 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 iowa_file_path = 'train.csv'
 
 # Fill in the line below to read the file into a variable home_data
-home_data = pd.read_csv(iowa_file_path)
-display(home_data.describe())
+df = pd.read_csv(iowa_file_path)
+print(df.describe())
+print(df.columns)
+
+#Target of prediction:
+y = df["SalePrice"]
+
+#Features:
+X = df[["LotArea","YearBuilt","1stFlrSF","2ndFlrSF","FullBath","BedroomAbvGr","TotRmsAbvGrd"]]
+
+#define the model:
+my_model = DecisionTreeRegressor(random_state=1)
+my_model.fit(X, y)
+print(X.head())
+print(my_model.predict(X.head()))
